5a3602542b39b8fc2872961da9f70fec57898e11
Babajide Oke <josua.oke@solera.com>
2022-10-22 18:23:10 +0100

    Practicing with states

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,25 +1,29 @@
 import "./ExpenseItem.css"
 import Date from "./Date";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 function ExpenseItem(props) {
+    const [initialTitle, setTitle] = useState(props.title);
 
-    //Spliting up Date details 
-
+    const clickHandler = () =>{
+        setTitle(initialTitle +' Updated versions')
+    }
 
     return (
         <Card className="expense-item">
             <div>
-                <Date date = {props.date} />
+                <Date date={props.date} />
             </div>
 
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{initialTitle}</h2>
             </div>
 
             <div className="expense-item__price">
                 ${props.amount}
             </div>
+            <button onClick={clickHandler}>Click me</button>
         </Card>
     );
 }
