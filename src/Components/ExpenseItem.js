e1d61223949d82f7f276d34e09014a88772b85be
Babajide Oke <josua.oke@solera.com>
2022-10-22 16:14:04 +0100

    Formatted date

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -2,13 +2,19 @@ import "./ExpenseItem.css"
 
 function ExpenseItem(props) {
 
-    // const expenseDate = Date(2021, 6, 21)
-    // const expenseTitle = "Car Insurance"
-    // const expenseAmount = 299;
+    //Spliting up Date details 
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
 
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
             </div>
